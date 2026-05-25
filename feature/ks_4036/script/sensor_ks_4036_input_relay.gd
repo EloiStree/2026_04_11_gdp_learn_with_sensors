eaaaf3bdbf89481edfc1acca3e7bd9991c4eb58c
Eloi Stree <eloistree@gmail.com>
2026-05-26 00:24:22 +0200

    Clean and unwarp some of the object

--- feature/ks_4036/script/sensor_ks_4036_input_relay.gd
+++ feature/ks_4036/script/sensor_ks_4036_input_relay.gd
@@ -1,20 +1,14 @@
 class_name SensorKs4036InputRelay
 extends Node
-
-
 static var instance_in_scene: SensorKs4036InputRelay = null
 static func get_instance():
 	return instance_in_scene
 
 signal on_joystick_input_received(input_joystick: Vector2)
-
 signal on_wheels_input_received(left_percent: float, right_percent: float)
 signal on_wheels_motors_input_received(top_left: bool, top_right: bool, bottom_left: bool, bottom_right: bool)
-
 signal on_color_led_front_left_updated(color: Color)
 signal on_color_led_front_right_updated(color: Color)
-signal on_color_led_under_car_updated(color_front_left: Color, color_front_right: Color, color_back_left: Color, color_back_right: Color)
-
 
 func _ready() -> void:
 	instance_in_scene = self
@@ -25,8 +19,6 @@ func _exit_tree() -> void:
 func clamp_percent(percent: float) -> float:
 	return clamp(percent, -1.0, 1.0)
 
-
-
 #region Joystick Input
 @export_group("Joystick Input")
 @export var last_joystick_input: Vector2 = Vector2.ZERO
@@ -36,7 +28,6 @@ func set_input_with_array(input_joystick: Vector2) -> void:
 	on_joystick_input_received.emit(stick)
 #endregion
 
-
 #region Wheels Percent Power Input
 @export_group("Wheels Input")
 @export var last_wheel_left_percent_11: float = 0.0
@@ -44,10 +35,8 @@ func set_input_with_array(input_joystick: Vector2) -> void:
 
 func set_left_wheel_percent_11(percent: float) -> void:
 	set_wheels_percent_11(percent, last_wheel_right_percent_11)
-
 func set_right_wheel_percent_11(percent: float) -> void:
 	set_wheels_percent_11(last_wheel_left_percent_11, percent)
-
 func set_wheels_percent_11(left_percent: float, right_percent: float) -> void:
 	last_wheel_left_percent_11 = clamp_percent(left_percent)
 	last_wheel_right_percent_11 = clamp_percent(right_percent)
@@ -79,17 +68,12 @@ func set_down_left_motor_button(set_motor_on: bool) -> void:
 
 func set_down_right_motor_button(set_motor_on: bool) -> void:
 	set_wheels_motors(last_top_left_motor, last_top_right_motor, last_down_left_motor, set_motor_on)
-
 #endregion
 
 #region LEDs Color
 @export_group("LEDs Color")
 @export var last_color_led_front_left: Color = Color(0, 0, 0)
 @export var last_color_led_front_right: Color = Color(0, 0,0)
-@export var last_color_led_under_car_front_left: Color = Color(0, 0, 0)
-@export var last_color_led_under_car_front_right: Color = Color(0, 0, 0)
-@export var last_color_led_under_car_back_left: Color = Color(0, 0, 0)
-@export var last_color_led_under_car_back_right: Color = Color(0, 0, 0)
 
 func set_color_led_front_left(color: Color) -> void:
 	last_color_led_front_left = color
@@ -98,12 +82,5 @@ func set_color_led_front_left(color: Color) -> void:
 func set_color_led_front_right(color: Color) -> void:
 	last_color_led_front_right = color
 	on_color_led_front_right_updated.emit(color)
-
-func set_color_led_under_car(color_front_left: Color, color_front_right: Color, color_back_left: Color, color_back_right: Color) -> void:
-	last_color_led_under_car_front_left = color_front_left
-	last_color_led_under_car_front_right = color_front_right
-	last_color_led_under_car_back_left = color_back_left
-	last_color_led_under_car_back_right = color_back_right
-	on_color_led_under_car_updated.emit(color_front_left, color_front_right, color_back_left, color_back_right)
 
 #endregion
